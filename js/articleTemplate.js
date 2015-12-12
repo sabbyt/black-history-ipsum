b0bd2f102d277f0544cb895467b6106406b50d0c
Ardian Ajvazi <ardian_46@hotmail.com>
2015-12-11 16:09:35 -0800

    show/hide bio

--- js/articleTemplate.js
+++ js/articleTemplate.js
@@ -2,12 +2,14 @@ var theTemplate;
 var content = {};
 
 var articleRun = function() {
+
   $.get('../template/template.handlebars', function(data) {
     theTemplate = Handlebars.compile(data);
   }).done(function() {
     content.authorList = authorData.map(theTemplate);
     content.authorList.forEach(function(el) {
       $('#authorBio').append(el);
+      hideArticles();
     });
   });
 };
